64baa2f9caa604c7c0c0a1942978ac44c945980b
Paula Molina <pau.molina@duocuc.cl>
2024-10-16 21:08:58 -0300

    storage config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,50 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { StorageService } from './services/storage.service';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+
+export function initApp(storageService: StorageService) {
+  return () => storageService.init();
+}
+
+export function routeReuseStrategy() {
+  return { provide: RouteReuseStrategy, useClass: IonicRouteStrategy };
+}
+
+export function storageService() {
+  return {
+    provide: APP_INITIALIZER,
+    useFactory: initApp,
+    deps: [StorageService],
+    multi: true,
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+  ],
+  providers: [
+    routeReuseStrategy(),
+    storageService(),
+    provideHttpClient(withInterceptorsFromDi()),
+    SQLite,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
